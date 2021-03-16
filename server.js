99d2c1f800de44862b7176915f89cd21bc5540c6
ry0y4n <rusk.ryoy4n@gmail.com>
2021-03-16 15:15:38 +0900

    fix: onMessage update3

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const { Server } = require('ws');
-const wss = new Server({ server });
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
